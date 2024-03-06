913065be0fa8bef01035a59efa5dda39d7a6e97e
Cooper Werner <cooperwerner824@gmail.com>
2024-03-06 12:23:05 -0500

    API Key API Working

--- amplify/backend/function/APIKeyManagerLambda/lib/app.ts
+++ amplify/backend/function/APIKeyManagerLambda/lib/app.ts
@@ -9,6 +9,13 @@ Amplify Params - DO NOT EDIT */
 import express from "express";
 import bodyParser from "body-parser";
 import awsServerlessExpressMiddleware from "aws-serverless-express/middleware";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+import {
+  getApiKey,
+  createApiKey,
+  deleteApiKey,
+  refreshApiKey,
+} from "./dbActions";
 
 // declare a new express app
 const app = express();
@@ -22,12 +29,105 @@ app.use(function (req, res, next) {
   next();
 });
 
+const cognitoJwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.COGNITO_USER_POOL_ID as string,
+  tokenUse: "access",
+  clientId: process.env.COGNITO_CLIENT_ID as string,
+});
+
 app.get("/", function (req, res) {
-  // Add your code here
   return res
     .status(200)
     .json({ message: "API Key Manager Online", status: 200 });
 });
 
+app.get("/getkey", async function (req, res) {
+  const jwt = req.headers.authorization;
+
+  if (!jwt) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: No JWT provided", status: 401 });
+  }
+
+  try {
+    cognitoJwtVerifier.verify(jwt);
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Invalid Cognito JWT", status: 401 });
+  }
+
+  if (!req.query.userId) {
+    return res
+      .status(400)
+      .json({ message: "Bad Request: Missing userId", status: 400 });
+  }
+
+  const apiKeyResponse = await getApiKey(req.query.userId as string);
+
+  if (apiKeyResponse.status === 404) {
+    const newApiKeyResponse = await createApiKey(req.query.userId as string);
+    return res.status(newApiKeyResponse.status).json(newApiKeyResponse);
+  }
+
+  return res.status(200).json(apiKeyResponse);
+});
+
+app.post("/refreshkey", async function (req, res) {
+  const jwt = req.headers.authorization;
+
+  if (!jwt) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: No JWT provided", status: 401 });
+  }
+
+  try {
+    cognitoJwtVerifier.verify(jwt);
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Invalid Cognito JWT", status: 401 });
+  }
+
+  if (!req.body.userId) {
+    return res
+      .status(400)
+      .json({ message: "Bad Request: Missing userId", status: 400 });
+  }
+
+  const apiKeyResponse = await refreshApiKey(req.body.userId as string);
+
+  return res.status(apiKeyResponse.status).json(apiKeyResponse);
+});
+
+app.delete("/deletekey", async function (req, res) {
+  const jwt = req.headers.authorization;
+
+  if (!jwt) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: No JWT provided", status: 401 });
+  }
+
+  try {
+    cognitoJwtVerifier.verify(jwt);
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Invalid Cognito JWT", status: 401 });
+  }
+
+  if (!req.query.userId) {
+    return res
+      .status(400)
+      .json({ message: "Bad Request: Missing userId", status: 400 });
+  }
+
+  const apiKeyResponse = await deleteApiKey(req.query.userId as string);
+
+  return res.status(apiKeyResponse.status).json(apiKeyResponse);
+});
 
 export default app;
---
+++ amplify/backend/function/APIKeyManagerLambda/lib/dbActions.ts
@@ -0,0 +1,84 @@
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  DeleteItemCommand,
+  type GetItemCommandInput,
+  type QueryCommandInput,
+  QueryCommand,
+} from "@aws-sdk/client-dynamodb";
+
+const REGION = process.env.REGION;
+const APIKEY_TABLE_NAME = process.env.STORAGE_APIKEYDB_NAME;
+
+const dbClient = new DynamoDBClient({ region: REGION });
+
+export async function getApiKey(userId: string) {
+  const params: QueryCommandInput = {
+    TableName: APIKEY_TABLE_NAME,
+    IndexName: "ByOwnerId",
+    Limit: 1,
+    KeyConditionExpression: "OwnerId = :ownerId",
+    ExpressionAttributeValues: {
+      ":ownerId": { S: userId },
+    },
+    ProjectionExpression: "OwnerId, APIKey",
+  };
+
+  const response = await dbClient.send(new QueryCommand(params));
+
+  if (response.Items && response.Items.length > 0) {
+    return { status: 200, apiKey: response.Items[0].APIKey.S };
+  } else {
+    return { status: 404, apiKey: null };
+  }
+}
+
+export async function createApiKey(userId: string) {
+  const apiKey = Math.random().toString(36).substring(2, 15);
+
+  const params = {
+    TableName: APIKEY_TABLE_NAME,
+    Item: {
+      OwnerId: { S: userId },
+      APIKey: { S: apiKey },
+    },
+  };
+
+  const response = await dbClient.send(new PutItemCommand(params));
+
+  if (response.$metadata.httpStatusCode !== 200) {
+    console.log("Issue creating API Key");
+    console.error(response);
+    return { status: 500, apiKey: null };
+  }
+
+  return { status: 200, apiKey: apiKey };
+}
+
+export async function deleteApiKey(userId: string) {
+  const params = {
+    TableName: APIKEY_TABLE_NAME,
+    Key: {
+      OwnerId: { S: userId },
+    },
+  };
+
+  const response = await dbClient.send(new DeleteItemCommand(params));
+
+  if (response.$metadata.httpStatusCode !== 200) {
+    console.log("Issue deleting API Key");
+    console.error(response);
+    return { status: 500 };
+  }
+
+  return { status: 204 };
+}
+
+export async function refreshApiKey(userId: string) {
+  const response = await deleteApiKey(userId);
+  if (response.status !== 204) {
+    return response;
+  }
+  return createApiKey(userId);
+}
